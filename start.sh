9f189b8e9ef52981466912d1e9b9dda2d0bf60ca
ngfchl <ngfchl@126.com>
2022-08-25 09:49:27 +0800

    1. 优化网站连接失败错误返回信息 2. 继续优化在线更新功能 3. 优化MT个人数据获取规则

--- start.sh
+++ start.sh
@@ -5,16 +5,15 @@ CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -f ./db/db.sqlite3 ]; then
   echo "-- 初始化数据库 init database --"
   # 如果数据库存在，就不执行
-  mv db.sqlite3 ./db/db.sqlite3
+  python manage.py makemigrations &&
+    python manage.py migrate &&
+    python manage.py loaddata pt.json
+#  mv db.sqlite3 ./db/db.sqlite3
 fi
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
   touch $CONTAINER_ALREADY_STARTED
   echo "-- First container startup --"
   # 此处插入你要执行的命令或者脚本文件
-  # 安装依赖
-  #  mv db.sqlite3 ./db/sqlite3 &&
-  # 导入数据 person.json为自定义
-  #  python manage.py loaddata person.json &&
   # 创建超级用户
   DJANGO_SUPERUSER_USERNAME=$DJANGO_SUPERUSER_USERNAME \
     DJANGO_SUPERUSER_EMAIL=$DJANGO_SUPERUSER_EMAIL \
@@ -25,5 +24,5 @@ else
 fi
 
 python manage.py makemigrations &&
-python manage.py migrate &&
-  python manage.py runserver 0.0.0.0:8000 --noreload
+  python manage.py migrate &&
+  python manage.py runserver $DJANGO_WEB_PORT --noreload
