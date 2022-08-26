cbd53223aad788dc4a5e080e2b28d771a86bd223
ngfchl <ngfchl@126.com>
2022-08-26 13:19:22 +0800

    1. 优化芒果站部分规则 2. 优化部分返回信息 3. 更新更新脚本，更新前备份数据库，后缀+年月日时分秒，方便恢复 4. 更新日志

--- update.sh
+++ update.sh
@@ -1,3 +1,5 @@
+# 先备份数据库文件再拉取更新 $(date "+%Y%m%d%H%M%S")当前时间年月日时分秒
+cp /var/www/html/ptools/db/db.sqlite3 /var/www/html/ptools/db/db.sqlite3-$(date "+%Y%m%d%H%M%S")   &&
 git pull &&
   mv -f /var/www/html/ptools/start.sh /var/www/html/start.sh &&
   python manage.py makemigrations &&
