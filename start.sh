433c324c6f42589f9ba76b0d47de388dacc8a04a
ngfchl <ngfchl@126.com>
2022-09-24 15:29:42 +0800

    第一次开机写入U2的HOSTS

--- start.sh
+++ start.sh
@@ -4,20 +4,22 @@ python -m pip install --upgrade pip &&
   CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
   echo "-- First container startup --"
-  # 此处插入你要执行的命令或者脚本文件
-  git config --global init.defaultBranch master &&
+  # 写入U2的HOSTS
+  echo 172.64.153.252 u2.dmhy.org >>/etc/hosts &&
+    # 此处插入你要执行的命令或者脚本文件
+    git config --global init.defaultBranch master &&
     git init &&
     git remote add origin https://gitee.com/ngfchl/ptools &&
     # 设置拉取最新文件并覆盖
     git config pull.ff only &&
     git pull origin master
-  ls -l && pwd; \
-    #    git branch --set-upstream-to=origin/master master &&
-    pip install -r requirements.txt; \
-    python manage.py makemigrations; \
-    python manage.py migrate; \
-    python manage.py loaddata pt.json; \
-    touch $CONTAINER_ALREADY_STARTED;
+  ls -l && pwd
+  #    git branch --set-upstream-to=origin/master master &&
+  pip install -r requirements.txt
+  python manage.py makemigrations
+  python manage.py migrate
+  python manage.py loaddata pt.json
+  touch $CONTAINER_ALREADY_STARTED
 
   # 创建超级用户
   DJANGO_SUPERUSER_USERNAME=$DJANGO_SUPERUSER_USERNAME \
