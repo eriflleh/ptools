0c646de570c4fd1c5c05c9235f680504a443d444
ngfchl <ngfchl@126.com>
2022-09-25 22:16:11 +0800

    U2的HOSTS改为每次启动写入一次，似乎docker每次重启HOSTS呗重置了？

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # 升级pip到最新
 python -m pip install --upgrade pip &&
+  # 写入U2的HOSTS
+  echo 172.64.153.252 u2.dmhy.org >> /etc/hosts &&
   CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
   echo "-- First container startup --"
-  # 写入U2的HOSTS
-  echo 172.64.153.252 u2.dmhy.org >>/etc/hosts &&
-    # 此处插入你要执行的命令或者脚本文件
-    git config --global init.defaultBranch master &&
+  # 此处插入你要执行的命令或者脚本文件
+  git config --global init.defaultBranch master &&
     git init &&
     git remote add origin https://gitee.com/ngfchl/ptools &&
     # 设置拉取最新文件并覆盖
