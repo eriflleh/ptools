c8a60088df9e552377cb49da1f2145c7961f26d9
ngfchl <ngfchl@126.com>
2022-09-25 22:00:13 +0800

    更新学校信息抓取规则

--- pt_site/UtilityTool.py
+++ pt_site/UtilityTool.py
@@ -1028,7 +1028,7 @@ class PtSpider:
                 return CommonResponse.success(data=(new_count, count))
         except Exception as e:
             # raise
-            self.send_text(site.name + '解析种子信息：失败！原因：' + str(e))
+            # self.send_text(site.name + '解析种子信息：失败！原因：' + str(e))
             return CommonResponse.error(msg='解析种子页面失败！' + str(e))
 
     # 从种子详情页面爬取种子HASH值
@@ -1116,7 +1116,7 @@ class PtSpider:
         except Exception as e:
             message = my_site.site.name + '访问个人主页信息：失败！原因：' + str(e)
             logging.error(message)
-            self.send_text(message)
+            # self.send_text(message)
             # raise
             return CommonResponse.error(msg=message)
 
@@ -1336,7 +1336,7 @@ class PtSpider:
                 message = my_site.site.name + '解析个人主页信息：失败！原因：' + str(e)
                 logging.error(message)
                 # raise
-                self.send_text('# <font color="red">' + message + '</font>  \n')
+                # self.send_text('# <font color="red">' + message + '</font>  \n')
                 return CommonResponse.error(msg=message)
 
     def get_hour_sp(self, my_site: MySite):
@@ -1347,24 +1347,24 @@ class PtSpider:
                 my_site=my_site,
                 url=site.url + site.page_mybonus,
             )
-            """
+            # """
             if 'btschool' in site.url:
-            # print(response.content.decode('utf8'))
-            url = self.parse(response, '//form[@id="challenge-form"]/@action[1]')
-            data = {
-                'md': ''.join(self.parse(response, '//form[@id="challenge-form"]/input[@name="md"]/@value')),
-                'r': ''.join(self.parse(response, '//form[@id="challenge-form"]/input[@name="r"]/@value'))
-            }
-            print(data)
-            print('学校时魔页面url：', url)
-            response = self.send_request(
-                my_site=my_site,
-                url=site.url + ''.join(url).lstrip('/'),
-                method='post',
-                # headers=headers,
-                data=data
-            )
-            """
+                # print(response.content.decode('utf8'))
+                url = self.parse(response, '//form[@id="challenge-form"]/@action[1]')
+                data = {
+                    'md': ''.join(self.parse(response, '//form[@id="challenge-form"]/input[@name="md"]/@value')),
+                    'r': ''.join(self.parse(response, '//form[@id="challenge-form"]/input[@name="r"]/@value'))
+                }
+                print(data)
+                print('学校时魔页面url：', url)
+                response = self.send_request(
+                    my_site=my_site,
+                    url=site.url + ''.join(url).lstrip('/'),
+                    method='post',
+                    # headers=headers,
+                    data=data
+                )
+            # """
             res = converter.convert(response.content)
             # print('时魔响应', response.content)
             # print('转为简体的时魔页面：', str(res))
